00642042166be953289d64b67d7ab3cd8b0e2c47
AxzelBC <alejo13580@gmail.com>
2021-09-30 15:07:27 -0500

    Revisión de funcionamiento del script.

--- VM-script.sh
+++ VM-script.sh
@@ -9,6 +9,7 @@
 # Fecha: 26/09/2021.
 #
 # Autor: @Axzel {alejo13580@gmail.com}
+# Autor: @Akira
 #
 ###############################################################################################
 
@@ -18,7 +19,7 @@ VM='Arch-Linux-32bit'
 
 
 # Creación de un disco dínamico de 32Gb
-VBoxManage createhd --filename $VM.vdi --size 32768
+VBoxManage createhd --filename $VM.vdi --size 20480
 
 
 # Creación de la máquina virtual.
@@ -26,10 +27,12 @@ VBoxManage createvm --name $VM --ostype "ArchLinux" --register
 
 
 # Se agrega un controldaor SATA con el controlador del disco adjunto.
-VBoxManage storagectl $VM --name "SATA Controller" --add sata \
->  --controller IntelAHCI
-VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 \
->  --device 0 --type hdd --medium $VM.vdi
+VBoxManage storagectl $VM --name "SATA Controller" --add sata --controller IntelAHCI
+VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM.vdi
+
+# Adición de controlador de disco para instalar SO.
+VBoxManage storagectl $VM --name "IDE Controller" --add ide
+VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium archlinux32-2021.09.01-i686.iso
 
 
 # Configuración general del sistema.
@@ -39,13 +42,13 @@ VBoxManage modifyvm $VM --ioapic on
 # Configuración de booteo de la BIOS.
 VBoxManage modifyvm $VM --boot1 dvd --boot2 disk --boot3 none --boot4 none
 # Configuración de la tarjera y adaptadores de Red.
-VBoxManage modifyvm $VM --nic1 bridged --bridgeadapter1 e1000g0
+VBoxManage modifyvm $VM --nic1 bridged --bridgeadapter1 eno1
 # Memoria RAM y memoria de vídeo.
-VBoxManage modifyvm $VM --memory 1024 --vram 128
+VBoxManage modifyvm $VM --memory 1024 --vram 64
 
 
-# Comando 
-VBoxHeadless -s $VM
+# Comando
+# VBoxHeadless -s $VM
 
 # VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 \
 # >  --device 0 --type dvddrive --medium none
